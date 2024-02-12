3eaa7bcb19b30bf0f7b8f2227210987bb1f1efb1
Latte <bjwq2019@gmail.com>
2024-02-12 17:08:07 -0500

    finish stuck and tahnk

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,13 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 // import Item from './qcomps/recipes.js'
-import Gallery from './qcomps/state.js'
+import FeedbackForm from './qcomps/thankYouCrash.js'
 // import Gallery from './components/state.js'
 
 export default function List() {
   return (
     <div className={styles.main}>
-        <Gallery />
+        <FeedbackForm />
     </div>
   )
 }
